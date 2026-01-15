045fe3df53d29a7e1a763be0d94b5b579d54222f
arunava6 <arunavasaharoy@gmail.com>
2026-01-16 00:17:38 +0530

    modified code

---
+++ src/main/java/com/example/IndiChessBackend/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.IndiChessBackend.service;
+
+import com.example.IndiChessBackend.model.User;
+import com.example.IndiChessBackend.model.UserPrincipal;
+import com.example.IndiChessBackend.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.getUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
